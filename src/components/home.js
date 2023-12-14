8045448da20fd1d16ecd15369488b3e353f70b79
MCantillo5 <marielacantillo0516@gmail.com>
2023-12-14 16:06:02 -0500

    media-actualizacion

--- src/components/home.js
+++ src/components/home.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React from 'react';
 
+const Home = () => {
+	return (
 
-
-const Home= () =>{
-  return(
-    <header className="header">
-      <div className="title">
-        <h1>ESTE ES EL PRIMER <br></br>PROYECTO DE REACT</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipiscing,  elit mus habitasse <br/> tempor natoque ut, vel dapibus torquent lobortis velit.</p>
+			<div className="content-home">
+			<header className="header">
+				<div className="title">
+					<h1>
+						ESTE ES EL PRIMER <br></br>PROYECTO DE REACT
+					</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipiscing, elit mus
+						habitasse <br /> tempor natoque ut, vel dapibus torquent lobortis
+						velit.
+					</p>
+				</div>
+			</header>
+      <section className="group-chart">
+        <img src='img/icon.png' className="icons" alt="img"></img>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing, sit amet consectetur adipiscing</p>
+      </section>
+      <section className="group-chart">
+        <img src='img/icon.png' className="icons" alt="img"></img>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing, sit amet consectetur adipiscing</p>
+      </section>
+      <section className="group-chart">
+        <img src='img/icon.png' className="icons" alt="img"></img>
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing, sit amet consectetur adipiscing</p>
+      </section>
       </div>
-    </header>
-  )
-}
+
+	);
+};
 
 export default Home;
